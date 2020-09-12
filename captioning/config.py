ac75946013ab3df34127716c87b28a9999d1ba94
Shubhi Agarwal <shubhi@Shubhis-MacBook-Air.local>
2020-09-12 19:32:18 -0400

    Train captioning encoder-decoder

--- captioning/config.py
+++ captioning/config.py
@@ -4,21 +4,23 @@ import os
 class Config:
     """Base config."""
 
-    data_dir = "/Users/dibu/Gatech Academics/ImageQuery/captioning/data/Flickr_Data/Flickr_Data"
-    annotations_file = os.path.join(data_dir, "Flickr_TextData/Flickr8k.token.txt")
-    images_dir = os.path.join(data_dir, "Images")
-    train_id_file = os.path.join(data_dir, "Flickr_TextData/Flickr_8k.trainImages.txt")
-    val_id_file = os.path.join(data_dir, "Flickr_TextData/Flickr_8k.devImages.txt")
-    test_id_file = os.path.join(data_dir, "Flickr_TextData/Flickr_8k.testImages.txt")
-    vocab_file = "/Users/dibu/Gatech Academics/ImageQuery/captioning/data/vocab.pkl"
-    batch_size = 4  # batch size
+    data_dir = "./data"
+    annotations_file = os.path.join(data_dir, "Flickr8k_text/Flickr8k.token.txt")
+    images_dir = os.path.join(data_dir, "Flicker8k_Dataset")
+    train_id_file = os.path.join(data_dir, "Flickr8k_text/Flickr_8k.trainImages.txt")
+    val_id_file = os.path.join(data_dir, "Flickr8k_text/Flickr_8k.devImages.txt")
+    test_id_file = os.path.join(data_dir, "Flickr8k_text/Flickr_8k.testImages.txt")
+    vocab_file = "./data/vocab.pkl"
+    encoder_file = "./models/encoder-1.pth"
+    decoder_file = "./models/decoder-1.pth"
+    batch_size = 20  # batch size
     vocab_threshold = 2  # minimum word count threshold
     vocab_from_file = True  # if True, load existing vocab file
     embed_size = 300  # dimensionality of image and word embeddings
     hidden_size = 512  # number of features in hidden state of the RNN decoder
-    num_epochs = 3  # number of training epochs
+    num_epochs = 1  # number of training epochs
     save_every = 1  # determines frequency of saving model weights
-    print_every = 100  # determines window for printing average loss
+    print_every = 10  # determines window for printing average loss
     num_workers = 1
     log_file = (
         "training_log.txt"  # name of file with saved training loss and perplexity
--- captioning/train.py
+++ captioning/train.py
@@ -15,18 +15,27 @@
 #  ==================================================================
 from collections import Counter
 
+from captioning.architecture.encoder import EncoderCNN
+from captioning.architecture.decoder import DecoderRNN
 from captioning.config import Config
 from captioning.data_handler.data_loader import get_data_loader
 from captioning.utils import display_image
 from captioning.data_handler.utils import parse_flickr
 import numpy as np
 import torch.utils.data as data
+import torch.nn as nn
+import torch
+import math
+import sys
+import os
 
 
 def execute():
     # Step1: Load Data
     flickr_ann_dict = parse_flickr(Config.annotations_file)
     train_loader = get_data_loader(Config, flickr_ann_dict, mode="train")
+    val_loader = get_data_loader(Config, flickr_ann_dict, mode="val")
+    vocab_size = len(train_loader.dataset.vocab)
     # print("Total number of tokens in vocabulary:", len(train_loader.dataset.vocab))
     # print("Total number of tokens in vocabulary:", len(train_loader.dataset.vocab))
     # print(train_loader.dataset.vocab("ieowoqjf"))
@@ -48,15 +57,28 @@ def execute():
     # display_image(train_loader)
 
     # Step2: Define and Initialize Neural Net/ Model Class/ Hypothesis(H).
-    # encoder = EncoderCNN()
-    # decoder = DecoderRNN()
+    encoder = EncoderCNN(Config.embed_size)
+    decoder = DecoderRNN(Config.embed_size, Config.hidden_size, vocab_size)
 
+    # features = encoder(images)
+    #
+    # print('type(features):', type(features))
+    # print('features.shape:', features.shape)
+    #
+    # outputs = decoder(features, captions)
+    #
+    # print('type(outputs):', type(outputs))
+    # print('outputs.shape:', outputs.shape)
     # Step3: Define Loss Function and optimizer
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    params = list(decoder.parameters()) + list(encoder.embed.parameters())
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(params=params, lr=0.001, momentum=0.9)
 
     # Step4: Train the network.
-    # train(net, optimizer, criterion, train_loader)
+    encoder.load_state_dict(torch.load(Config.encoder_file))
+    decoder.load_state_dict(torch.load(Config.decoder_file))
+
+    train(encoder, decoder, optimizer, criterion, train_loader, val_loader)
     #
     # # Step5: Save Model
     # PATH = "./model/cifar_net.pth"
@@ -65,115 +87,123 @@ def execute():
     # # Step6: Test the net on Test Data
     # net = Net()
     # net.load_state_dict(torch.load(PATH))
-    # run_test(test_loader, net, classes)
+    # run_test(test_loader, net, classes
 
 
-# def train():
-#     losses = list()
-#     val_losses = list()
-#
-#     for epoch in range(1, 10 + 1):
-#
-#         for i_step in range(1, total_step + 1):
-#
-#             # zero the gradients
-#             decoder.zero_grad()
-#             encoder.zero_grad()
-#
-#             # set decoder and encoder into train mode
-#             encoder.train()
-#             decoder.train()
-#
-#             # Randomly sample a caption length, and sample indices with that length.
-#             indices = train_data_loader.dataset.get_train_indices()
-#
-#             # Create and assign a batch sampler to retrieve a batch with the sampled indices.
-#             new_sampler = data.sampler.SubsetRandomSampler(indices=indices)
-#             train_data_loader.batch_sampler.sampler = new_sampler
-#
-#             # Obtain the batch.
-#             images, captions = next(iter(train_data_loader))
-#
-#             # make the captions for targets and teacher forcer
-#             captions_target = captions[:, 1:].to(device)
-#             captions_train = captions[:, : captions.shape[1] - 1].to(device)
-#
-#             # Move batch of images and captions to GPU if CUDA is available.
-#             images = images.to(device)
-#
-#             # Pass the inputs through the CNN-RNN model.
-#             features = encoder(images)
-#             outputs = decoder(features, captions_train)
-#
-#             # Calculate the batch loss
-#             loss = criterion(
-#                 outputs.view(-1, vocab_size), captions_target.contiguous().view(-1)
-#             )
-#
-#             # Backward pass
-#             loss.backward()
-#
-#             # Update the parameters in the optimizer
-#             optimizer.step()
-#
-#             # - - - Validate - - -
-#             # turn the evaluation mode on
-#             with torch.no_grad():
-#
-#                 # set the evaluation mode
-#                 encoder.eval()
-#                 decoder.eval()
-#
-#                 # get the validation images and captions
-#                 val_images, val_captions = next(iter(val_data_loader))
-#
-#                 # define the captions
-#                 captions_target = val_captions[:, 1:].to(device)
-#                 captions_train = val_captions[:, : val_captions.shape[1] - 1].to(device)
-#
-#                 # Move batch of images and captions to GPU if CUDA is available.
-#                 val_images = val_images.to(device)
-#
-#                 # Pass the inputs through the CNN-RNN model.
-#                 features = encoder(val_images)
-#                 outputs = decoder(features, captions_train)
-#
-#                 # Calculate the batch loss.
-#                 val_loss = criterion(
-#                     outputs.view(-1, vocab_size), captions_target.contiguous().view(-1)
-#                 )
-#
-#             # append the validation loss and training loss
-#             val_losses.append(val_loss.item())
-#             losses.append(loss.item())
-#
-#             # save the losses
-#             np.save("losses", np.array(losses))
-#             np.save("val_losses", np.array(val_losses))
-#
-#             # Get training statistics.
-#             stats = "Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Val Loss: %.4f" % (
-#                 epoch,
-#                 num_epochs,
-#                 i_step,
-#                 total_step,
-#                 loss.item(),
-#                 val_loss.item(),
-#             )
-#
-#             # Print training statistics (on same line).
-#             print("\r" + stats, end="")
-#             sys.stdout.flush()
-#
-#         # Save the weights.
-#         if epoch % save_every == 0:
-#             print("\nSaving the model")
-#             torch.save(
-#                 decoder.state_dict(), os.path.join("./models", "decoder-%d.pth" % epoch)
-#             )
-#             torch.save(
-#                 encoder.state_dict(), os.path.join("./models", "encoder-%d.pth" % epoch)
-#             )
+def train(encoder, decoder, optimizer, criterion, train_loader, val_loader):
+    losses = list()
+    val_losses = list()
+    vocab_size = len(train_loader.dataset.vocab)
+    total_step = math.ceil(len(train_loader.dataset.caption_lengths) / train_loader.batch_sampler.batch_size)
+
+    for epoch in range(1, Config.num_epochs+1):
+
+        for i_step in range(1, total_step + 1):
+
+            # zero the gradients
+            decoder.zero_grad()
+            encoder.zero_grad()
+
+            # set decoder and encoder into train mode
+            # encoder.train()
+            # decoder.train()
+
+            # Randomly sample a caption length, and sample indices with that length.
+            indices = train_loader.dataset.get_train_indices()
+
+            # Create and assign a batch sampler to retrieve a batch with the sampled indices.
+            new_sampler = data.sampler.SubsetRandomSampler(indices=indices)
+            train_loader.batch_sampler.sampler = new_sampler
+
+            # Obtain the batch.
+            images, captions = next(iter(train_loader))
+
+            # make the captions for targets and teacher forcer
+            #captions_target = captions[:, 1:]#.to(device)
+            #captions_train = captions[:, : captions.shape[1] - 1]#.to(device)
+
+            # Move batch of images and captions to GPU if CUDA is available.
+            # images = images.to(device)
+
+            # Pass the inputs through the CNN-RNN model.
+            features = encoder(images)
+            outputs = decoder(features, captions)
+
+            # Calculate the batch loss
+            loss = criterion(
+                #outputs.view(-1, vocab_size), captions_target.contiguous().view(-1)
+                outputs.view(-1, vocab_size), captions.view(-1)
+            )
+
+            # Backward pass
+            loss.backward()
+
+            # Update the parameters in the optimizer
+            optimizer.step()
+
+            # - - - Validate - - -
+            # turn the evaluation mode on
+            with torch.no_grad():
+
+                # set the evaluation mode
+                # encoder.eval()
+                # decoder.eval()
+                val_indices = val_loader.dataset.get_train_indices()
+
+                # Create and assign a batch sampler to retrieve a batch with the sampled indices.
+                val_sampler = data.sampler.SubsetRandomSampler(indices=val_indices)
+                val_loader.batch_sampler.sampler = val_sampler
+                # get the validation images and captions
+                val_images, val_captions = next(iter(val_loader))
+
+                # define the captions
+                # captions_target = val_captions[:, 1:]#.to(device)
+                # captions_train = val_captions[:, : val_captions.shape[1] - 1]#.to(device)
+
+                # Move batch of images and captions to GPU if CUDA is available.
+                # val_images = val_images.to(device)
+
+                # Pass the inputs through the CNN-RNN model.
+                features = encoder(val_images)
+                outputs = decoder(features, val_captions)
+
+                # Calculate the batch loss.
+                val_loss = criterion(
+                    outputs.view(-1, vocab_size), val_captions.view(-1)
+                )
+
+            # append the validation loss and training loss
+            val_losses.append(val_loss.item())
+            losses.append(loss.item())
+
+            # save the losses
+            np.save("losses", np.array(losses))
+            np.save("val_losses", np.array(val_losses))
+
+            # Get training statistics.
+            stats = "Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Val Loss: %.4f" % (
+                epoch,
+                Config.num_epochs,
+                i_step,
+                total_step,
+                loss.item(),
+                val_loss.item(),
+            )
+
+            # Print training statistics (on same line).
+            if i_step % Config.print_every == 0:
+                print("\r" + stats, end="")
+                # sys.stdout.flush()
+
+        # Save the weights.
+        if epoch % Config.save_every == 0:
+            print("\nSaving the model")
+            torch.save(
+                decoder.state_dict(), os.path.join("./models", "decoder-%d.pth" % epoch)
+            )
+            torch.save(
+                encoder.state_dict(), os.path.join("./models", "encoder-%d.pth" % epoch)
+            )
 
 
 if __name__ == "__main__":
