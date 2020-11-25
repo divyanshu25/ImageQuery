84f0c1658a376e43c8ac78854025597bfbd3a0d3
Harish Krupo <harishkrupo@gmail.com>
2020-11-25 03:24:45 +0000

    Add bert support for views
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

--- captioning/views.py
+++ captioning/views.py
@@ -38,6 +38,7 @@ import torch.nn.functional as F
 
 stopset = set(stopwords.words("english"))
 
+config = Config()
 
 @doc(
     summary="Load image captions",
@@ -245,13 +246,19 @@ class SearchImage(Resource):
 
         return decoder.embedding
 
-    def get_token_ids(self, query, vocab, is_filter=False):
+    def get_token_ids(self, query, vocab, is_filter=False, bert=None):
         caption = []
         tokens = self.filter_stopwords(
             nltk.tokenize.word_tokenize(str(query).lower()), is_filter
         )
         # print(f"Tokens after stop word removal: {tokens}")
-        caption.extend([vocab(token) for token in tokens])
+        if config.enable_bert:
+            tokenizer = bert.get_tokenizer()
+            caption.extend(
+                [tokenizer.convert_tokens_to_ids(token) for token in tokens]
+            )
+        else:
+            caption.extend([vocab(token) for token in tokens])
         return torch.tensor(caption)
 
     @marshal_with(PopulateSearchSchema, code=200)
@@ -265,8 +272,9 @@ class SearchImage(Resource):
             return make_response(dict(status="Invalid set or model name"), 500)
 
         config = Config()
-        vocab = get_vocabulary(config, type=config.dataset_type, bert=None)
-        embedding_layer = self.get_embedding_layer(config, len(vocab))
+        bert = BERT()
+        vocab = get_vocabulary(config, type=config.dataset_type, bert=bert)
+        embedding_layer = self.get_embedding_layer(config, len(vocab), bert=bert)
 
         data = (
             db.session.query(ImageCaptions)
@@ -281,8 +289,8 @@ class SearchImage(Resource):
                 unpadded_caption = d.caption.rstrip(" .")
                 unpadded_caption += " ."
                 # print(f"Query: {query}, Caption: {unpadded_caption}")
-                token_id_query = self.get_token_ids(query, vocab, filter)
-                token_id_caption = self.get_token_ids(unpadded_caption, vocab, filter)
+                token_id_query = self.get_token_ids(query, vocab, filter, bert=bert)
+                token_id_caption = self.get_token_ids(unpadded_caption, vocab, filter, bert=bert)
                 # print(f"Query_token_id: {token_id_query}, Caption_token_id: {token_id_caption}")
                 # print(f"Query_token_em: {embedding_layer(token_id_query).shape}, Caption_token_em: {embedding_layer(token_id_caption).shape}")
                 # print(f"Query_token_norm: {torch.norm(embedding_layer(token_id_query), p=2, dim=1).shape}, "
@@ -306,10 +314,9 @@ class SearchImage(Resource):
                 # similarity_val = torch.mean(
                 #         F.relu(torch.matmul(caption_embedding, query_embedding.t()))
                 # )
-
-                similarity_val = torch.mean(
-                        torch.matmul(caption_embedding, query_embedding.t())
-                )
+                mml = torch.matmul(caption_embedding, query_embedding.t())
+                _, S, _ = torch.svd(mml)
+                similarity_val = S[0]
                 if d.image_path not in similarity_scores:
                     similarity_scores[d.image_path] = 0
                 similarity_scores[d.image_path] = max(
